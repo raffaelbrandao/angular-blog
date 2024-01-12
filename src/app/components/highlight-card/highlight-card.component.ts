78985fab8ed03cb92ae972475e2048882cfbcfb5
Rafael Brandão <raffaelbrandao@gmail.com>
2024-01-11 21:45:06 -0300

    Create highlight card component

---
+++ src/app/components/highlight-card/highlight-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-highlight-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './highlight-card.component.html',
+  styleUrl: './highlight-card.component.css'
+})
+export class HighlightCardComponent {
+
+}
