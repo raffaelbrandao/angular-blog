2a7e55e29f896dda7e7bcba1973bd157a3caba08
Rafael Brandão <raffaelbrandao@gmail.com>
2024-01-24 14:02:54 -0300

    Add params components

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-
+  @Input()
+  image: string = '';
+  @Input()
+  title: string = '';
+  constructor() {}
 }
--- src/app/components/highlight-card/highlight-card.component.ts
+++ src/app/components/highlight-card/highlight-card.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-highlight-card',
   standalone: true,
   imports: [],
   templateUrl: './highlight-card.component.html',
-  styleUrl: './highlight-card.component.css'
+  styleUrl: './highlight-card.component.css',
 })
 export class HighlightCardComponent {
-
+  @Input()
+  image: string = '';
+  @Input()
+  title: string = '';
+  @Input()
+  sumary: string = '';
+  constructor() {}
 }
