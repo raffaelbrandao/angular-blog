306dd2cc73b7096f4d38bb432d098460042e638a
Rafael Brandão <raffaelbrandao@gmail.com>
2024-01-24 13:35:48 -0300

    Create menu bar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderTitleComponent } from "../../components/header-title/header-title.component";
 import { HighlightCardComponent } from "../../components/highlight-card/highlight-card.component";
 import { CardComponent } from "../../components/card/card.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeaderTitleComponent, HighlightCardComponent, CardComponent]
+    imports: [HeaderTitleComponent, HighlightCardComponent, CardComponent, MenuBarComponent]
 })
 export class HomeComponent {
 
